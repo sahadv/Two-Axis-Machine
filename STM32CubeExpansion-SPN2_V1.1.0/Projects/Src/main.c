f85c51f86102ced8d8049c8f64a7329fe9813a8d
Feilan Jiang <fjiang120@gmail.com>
2019-07-20 21:28:40 -0400

    add D3 speed control
    - add range map function
    - refactor some code to allow for separate speed control for two motors

--- STM32CubeExpansion-SPN2_V1.1.0/Projects/Src/main.c
+++ STM32CubeExpansion-SPN2_V1.1.0/Projects/Src/main.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 
 #include "xnucleoihm02a1.h"
 #include "example.h"
@@ -13,11 +14,27 @@
 #define LIM_SWITCH_H_R GPIO_PIN_0 // PA0
 #define LIM_SWITCH_V_T GPIO_PIN_10 // PA10
 #define LIM_SWITCH_V_B GPIO_PIN_1 // PA1
-#define ADC_PIN GPIO_PIN_0 // PC0
 #define DEFAULT_SPEED_H  15000
 #define DEFAULT_SPEED_V  20000
 
 #define NUCLEO_USE_USART
+
+#define ADC_H_PIN GPIO_PIN_0 // PC0
+#define POT_H_ACTUAL_MIN 94
+#define POT_H_ACTUAL_MAX 3366
+#define MOTOR_H_MIN -40000
+#define MOTOR_H_MAX 40000
+#define MOTOR_H_DEADBAND 10000
+
+/* #define USING_V_AXIS */
+#ifdef USING_V_AXIS
+#define ADC_V_PIN GPIO_PIN_0 // TODO change to something else if doing bonus
+#define POT_V_ACTUAL_MIN 94
+#define POT_V_ACTUAL_MAX 3366
+#define MOTOR_V_MIN -40000
+#define MOTOR_V_MAX 40000
+#define MOTOR_V_DEADBAND 10000
+#endif
 
 __IO uint16_t uhADCxConvertedValue = 0;
 eL6470_DirId_t direction_h = L6470_DIR_FWD_ID;
@@ -25,11 +42,13 @@ eL6470_DirId_t direction_v = L6470_DIR_FWD_ID;
 int8_t motor_h_id = 0;
 int8_t motor_v_id = 1;
 
-uint32_t speed_h = DEFAULT_SPEED_H;
-uint32_t speed_v = DEFAULT_SPEED_V;
+int32_t speed_h = DEFAULT_SPEED_H;
+int32_t speed_v = DEFAULT_SPEED_V;
+
+ADC_HandleTypeDef adc1, adc2;
 
 static void Error_Handler(void);
-uint16_t Read_ADC(void);
+uint16_t Read_ADC(ADC_HandleTypeDef *hadc);
 
 uint8_t buf[15];
 
@@ -73,10 +92,17 @@ void lim_switch_init(void) {
     HAL_NVIC_EnableIRQ(EXTI1_IRQn);
     
     // ADC
-    GPIO_InitStruct.Pin = ADC_PIN;
+    GPIO_InitStruct.Pin = ADC_H_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+#ifdef USING_V_AXIS
+    GPIO_InitStruct.Pin = ADC_V_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+#endif
 }
 
 void EXTI9_5_IRQHandler(void) {
@@ -129,72 +155,50 @@ void EXTI1_IRQHandler(void)
     }
 }
 
-
-/*
- * static void ADCx_Init(void)
- * {
- *   if(HAL_ADC_GetState(&hnucleo_Adc) == HAL_ADC_STATE_RESET)
- *   {
- *     [> ADC Config <]
- *     hnucleo_Adc.Instance                   = NUCLEO_ADCx;
- *     hnucleo_Adc.Init.ClockPrescaler        = ADC_CLOCKPRESCALER_PCLK_DIV4; [> (must not exceed 36MHz) <]
- *     hnucleo_Adc.Init.Resolution            = ADC_RESOLUTION12b;
- *     hnucleo_Adc.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
- *     hnucleo_Adc.Init.ContinuousConvMode    = DISABLE;
- *     hnucleo_Adc.Init.DiscontinuousConvMode = DISABLE;
- *     hnucleo_Adc.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
- *     hnucleo_Adc.Init.EOCSelection          = EOC_SINGLE_CONV;
- *     hnucleo_Adc.Init.NbrOfConversion       = 1;
- *     hnucleo_Adc.Init.DMAContinuousRequests = DISABLE;    **************************************************
- *
- *     ADCx_MspInit(&hnucleo_Adc);
- *     HAL_ADC_Init(&hnucleo_Adc);
- *   }
- * }
- */
 void ADC_Init(void) {
-  ADC_ChannelConfTypeDef sConfig;
+    ADC_ChannelConfTypeDef sConfig;
 
-  /* GPIO Ports Clock Enable */
-  __GPIOB_CLK_ENABLE();
+    __GPIOB_CLK_ENABLE();
 
-    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
-    */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV4;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = ENABLE;
-  hadc1.Init.ContinuousConvMode = ENABLE;
-  // hadc1.Init.DiscontinuousConvMode = ; // discarded because ScanConvMode set to ENABLE
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 2;   // 1 channel per axis
-  hadc1.Init.DMAContinuousRequests = ENABLE;    // TODO may need to set DMA to circular mode (see following 3 lines)
-  /* ADC_MultiModeTypeDef multi; */
-  /* multi.Mode =  */
-  /* HAL_ADCEx_MultiModeConfigChannel(&hacd1,  */
-  hadc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;   // EOC at end of sequence
-  HAL_ADC_Init(&hadc1);
+    adc1.Instance = ADC1;
+    adc1.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV4;
+    adc1.Init.Resolution = ADC_RESOLUTION_12B;
+    adc1.Init.ScanConvMode = ENABLE;
+    adc1.Init.ContinuousConvMode = ENABLE;
+    adc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    adc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    adc1.Init.NbrOfConversion = 2;
+    adc1.Init.DMAContinuousRequests = ENABLE;
+    adc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;
+    HAL_ADC_Init(&adc1);
+    sConfig.Channel = ADC_CHANNEL_10;
+    sConfig.Rank = 1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_84CYCLES;
+    sConfig.Offset = 1;
+    HAL_ADC_ConfigChannel(&adc1, &sConfig);
 
-    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
-    */
-  sConfig.Channel = ADC_CHANNEL_10;		/* Currently set to input pin PB0, adjust as needed */
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_84CYCLES;
-  sConfig.Offset = 1;
-  HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+#ifdef USING_V_AXIS
+    adc2.Instance = ADC1;
+    adc2.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV4;
+    adc2.Init.Resolution = ADC_RESOLUTION_12B;
+    adc2.Init.ScanConvMode = ENABLE;
+    adc2.Init.ContinuousConvMode = ENABLE;
+    adc2.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    adc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    adc2.Init.NbrOfConversion = 2;
+    adc2.Init.DMAContinuousRequests = ENABLE;
+    adc2.Init.EOCSelection = ADC_EOC_SEQ_CONV;
+    HAL_ADC_Init(&adc2);
+    sConfig.Channel = ADC_CHANNEL_10;   // TODO if bonus then change channel to something else
+    sConfig.Rank = 1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_84CYCLES;
+    sConfig.Offset = 1;
+    HAL_ADC_ConfigChannel(&adc2, &sConfig);
+#endif
 }
 
-int GetCalibratedADC(uint32_t uncalibrated) {
-    return 4095*(uncalibrated-94)/(3366-94);
-}
-
-int GetSpeedFromCalibratedADC(uint32_t calibrated) {
-    return (80000*calibrated)/4095-40000;
-}
-
-uint32_t GetSpeedFromUncalibratedADC(uint32_t uncalibrated) {
-    return GetSpeedFromCalibratedADC(GetCalibratedADC(uncalibrated));
+int32_t map(int32_t val, int32_t lo, int32_t hi, int32_t new_lo, int32_t new_hi) {
+    return (float) (val - lo) / (hi - lo) * (new_hi - new_lo) + new_lo;
 }
 
 int main(void)
@@ -206,47 +210,52 @@ int main(void)
     /* X-NUCLEO-IHM02A1 initialization */
     BSP_Init();
 
-    /* Fill the L6470_DaisyChainMnemonic structure */
     Fill_L6470_DaisyChainMnemonic();
-    /*Initialize the motor parameters */
     Motor_Param_Reg_Init();
 
     ADC_Init();
 
-    /*
-     * initialize limit switch pins with interrupt handlers
-     */
     lim_switch_init();
 
+    // set speeds again after interrupts triggered
     speed_h = DEFAULT_SPEED_H;
     speed_v = DEFAULT_SPEED_V;
   
-    // test program that moves motors back and forth, checking for limits when needed
     while (1) {
-        USART_CheckAppCmd();  // read motor commands
+        speed_h = map(Read_ADC(&adc1),
+                      POT_H_ACTUAL_MIN,
+                      POT_H_ACTUAL_MAX,
+                      MOTOR_H_MIN,
+                      MOTOR_H_MAX);
+        if (abs(speed_h) < MOTOR_H_DEADBAND) {
+            speed_h = 0;
+        }
+        direction_h = (speed_h < 0) ? L6470_DIR_REV_ID : L6470_DIR_FWD_ID;
+        BSP_L6470_Run(0, motor_h_id, direction_h, abs(speed_h));
 
-        memset(buf, 0, sizeof(buf));
-        num2str(Read_ADC(), buf);
-        USART_Transmit(&huart2, buf);
-        USART_Transmit(&huart2, "\r\n");
+        /* memset(buf, 0, sizeof(buf)); */
+        /* sprintf(buf, "motor val: %d\r\n", speed_h); */
+        /* USART_Transmit(&huart2, buf); */
 
-        BSP_L6470_Run(0, motor_h_id, direction_h, speed_h);
-        BSP_L6470_Run(0, motor_v_id, direction_v, speed_v);
+#ifdef USING_V_AXIS
+        speed_v = map(Read_ADC(&adc2),
+                      POT_V_ACTUAL_MIN,
+                      POT_V_ACTUAL_MAX,
+                      MOTOR_V_MIN,
+                      MOTOR_V_MAX);
+        if (abs(speed_v) < MOTOR_V_DEADBAND) {
+            speed_v = 0;
+        }
+        direction_v = (speed_v < 0) ? L6470_DIR_REV_ID : L6470_DIR_FWD_ID;
+        BSP_L6470_Run(0, motor_v_id, direction_v, abs(speed_v));
+#endif
     }
 }
 
-/**
- * @brief  This function is executed in case of error occurrence.
- * @param  None
- * @retval None
- */
-static void Error_Handler(void)
-{
+static void Error_Handler(void) {
     /* Turn LED2 on */
     BSP_LED_On(LED2);
-    while (1)
-    {
-    }
+    while (1) { }
 }
 
 #ifdef USE_FULL_ASSERT
@@ -258,8 +267,7 @@ static void Error_Handler(void)
  * @param line: assert_param error line source number
  * @retval None
  */
-void assert_failed(uint8_t* file, uint32_t line)
-{
+void assert_failed(uint8_t* file, uint32_t line) {
     /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
@@ -273,11 +281,10 @@ ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
  * @brief  This function return the ADC conversion result.
  * @retval The number into the range [0, 4095] as [0, 3.3]V.
  */
-uint16_t Read_ADC(void)
-{
-    HAL_ADC_Start(&HADC);
-    HAL_ADC_PollForConversion(&HADC, 100);
+uint16_t Read_ADC(ADC_HandleTypeDef *hadc) {
+    HAL_ADC_Start(hadc);
+    HAL_ADC_PollForConversion(hadc, 100);
 
-    return HAL_ADC_GetValue(&HADC);
+    return HAL_ADC_GetValue(hadc);
 }
 
