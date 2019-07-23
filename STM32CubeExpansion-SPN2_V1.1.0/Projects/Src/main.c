9699755a358ba8b4fd089c70a00c0cbf76815f43
Feilan Jiang <fjiang120@gmail.com>
2019-07-23 16:02:31 -0400

    add speed control and limit switch control

--- STM32CubeExpansion-SPN2_V1.1.0/Projects/Src/main.c
+++ STM32CubeExpansion-SPN2_V1.1.0/Projects/Src/main.c
@@ -50,10 +50,13 @@ ADC_HandleTypeDef adc1, adc2;
 static void Error_Handler(void);
 uint16_t Read_ADC(ADC_HandleTypeDef *hadc);
 
-uint8_t buf[15];
+uint8_t buf[40];
 
 void lim_switch_init(void) {
     GPIO_InitTypeDef GPIO_InitStruct;
+
+    __GPIOA_CLK_ENABLE();
+    __GPIOC_CLK_ENABLE();
 
     // H_L
     GPIO_InitStruct.Pin = LIM_SWITCH_H_L;
@@ -61,8 +64,6 @@ void lim_switch_init(void) {
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
     // H_R
     GPIO_InitStruct.Pin = LIM_SWITCH_H_R;
@@ -70,8 +71,6 @@ void lim_switch_init(void) {
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 
     // V_T
     GPIO_InitStruct.Pin = LIM_SWITCH_V_T;
@@ -79,8 +78,6 @@ void lim_switch_init(void) {
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
     // V_B
     GPIO_InitStruct.Pin = LIM_SWITCH_V_B;
@@ -88,8 +85,6 @@ void lim_switch_init(void) {
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(EXTI1_IRQn);
     
     // ADC
     GPIO_InitStruct.Pin = ADC_H_PIN;
@@ -105,60 +100,8 @@ void lim_switch_init(void) {
 #endif
 }
 
-void EXTI9_5_IRQHandler(void) {
-    if (__HAL_GPIO_EXTI_GET_IT(LIM_SWITCH_H_L) != RESET) {
-        BSP_L6470_HardStop(0, motor_h_id);
-        speed_h = 0;
-        __HAL_GPIO_EXTI_CLEAR_IT(LIM_SWITCH_H_L);
-    }
-}
-
-void EXTI0_IRQHandler(void)
-{
-    if (__HAL_GPIO_EXTI_GET_IT(LIM_SWITCH_H_R) != RESET) {
-        BSP_L6470_HardStop(0, motor_h_id);
-        speed_h = 0;
-        __HAL_GPIO_EXTI_CLEAR_IT(LIM_SWITCH_H_R);
-    }
-
-    if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_0) != RESET) {
-        __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_0);
-        BSP_L6470_BusySynchEventManager();
-    }
-}
-
-void EXTI15_10_IRQHandler(void)
-{
-    if (__HAL_GPIO_EXTI_GET_IT(LIM_SWITCH_V_T) != RESET) {
-        BSP_L6470_HardStop(0, motor_v_id);
-        speed_v = 0;
-        __HAL_GPIO_EXTI_CLEAR_IT(LIM_SWITCH_V_T);
-    }
-
-    if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_13) != RESET) {
-        __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_13);
-        BSP_EmergencyStop();
-    }
-}
-
-void EXTI1_IRQHandler(void)
-{
-    if (__HAL_GPIO_EXTI_GET_IT(LIM_SWITCH_V_B) != RESET) {
-        BSP_L6470_HardStop(0, motor_v_id);
-        speed_v = 0;
-        __HAL_GPIO_EXTI_CLEAR_IT(LIM_SWITCH_V_B);
-    }
-
-    if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_1) != RESET) {
-        __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_1);
-        BSP_L6470_FlagEventManager();
-    }
-}
-
 void ADC_Init(void) {
     ADC_ChannelConfTypeDef sConfig;
-
-    __GPIOB_CLK_ENABLE();
 
     adc1.Instance = ADC1;
     adc1.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV4;
@@ -222,36 +165,61 @@ int main(void)
     speed_v = DEFAULT_SPEED_V;
   
     while (1) {
-        speed_h = map(Read_ADC(&adc1),
-                      POT_H_ACTUAL_MIN,
-                      POT_H_ACTUAL_MAX,
-                      MOTOR_H_MIN,
-                      MOTOR_H_MAX);
-        if (abs(speed_h) < MOTOR_H_DEADBAND) {
+        uint16_t adc1_val = Read_ADC(&adc1);
+        int switch_hr = HAL_GPIO_ReadPin(GPIOA, LIM_SWITCH_H_R);
+        int switch_hl = HAL_GPIO_ReadPin(GPIOA, LIM_SWITCH_H_L);
+        /* speed_h = map(Read_ADC(&adc1), */
+                      /* POT_H_ACTUAL_MIN, */
+                      /* POT_H_ACTUAL_MAX, */
+                      /* MOTOR_H_MIN, */
+                      /* MOTOR_H_MAX); */
+        speed_h = SpeedFilteringWithADCValue(adc1_val);
+        direction_h = (speed_h < 0) ? L6470_DIR_REV_ID : L6470_DIR_FWD_ID;
+        if (abs(speed_h) < MOTOR_H_DEADBAND
+            || (direction_h == L6470_DIR_FWD_ID && switch_hr != RESET)
+            || (direction_h == L6470_DIR_REV_ID && switch_hl != RESET)) {
             speed_h = 0;
         }
-        direction_h = (speed_h < 0) ? L6470_DIR_REV_ID : L6470_DIR_FWD_ID;
         BSP_L6470_Run(0, motor_h_id, direction_h, abs(speed_h));
 
-        /* memset(buf, 0, sizeof(buf)); */
-        /* sprintf(buf, "motor val: %d\r\n", speed_h); */
-        /* USART_Transmit(&huart2, buf); */
-
 #ifdef USING_V_AXIS
-        speed_v = map(Read_ADC(&adc2),
-                      POT_V_ACTUAL_MIN,
-                      POT_V_ACTUAL_MAX,
-                      MOTOR_V_MIN,
-                      MOTOR_V_MAX);
-        if (abs(speed_v) < MOTOR_V_DEADBAND) {
+        uint16_t adc2_val = Read_ADC(&adc2);
+        int switch_vt = HAL_GPIO_ReadPin(GPIOA, LIM_SWITCH_V_T);
+        int switch_vb = HAL_GPIO_ReadPin(GPIOA, LIM_SWITCH_V_B);
+        /* speed_v = map(Read_ADC(&adc2), */
+                      /* POT_V_ACTUAL_MIN, */
+                      /* POT_V_ACTUAL_MAX, */
+                      /* MOTOR_V_MIN, */
+                      /* MOTOR_V_MAX); */
+        speed_v = SpeedFilteringWithADCValue(adc2_val);
+        direction_v = (speed_v < 0) ? L6470_DIR_REV_ID : L6470_DIR_FWD_ID;
+        if (abs(speed_v) < MOTOR_V_DEADBAND
+            || (direction_v == L6470_DIR_FWD_ID && switch_vt != RESET)
+            || (direction_v == L6470_DIR_REV_ID && switch_vb != RESET)) {
             speed_v = 0;
         }
-        direction_v = (speed_v < 0) ? L6470_DIR_REV_ID : L6470_DIR_FWD_ID;
         BSP_L6470_Run(0, motor_v_id, direction_v, abs(speed_v));
 #endif
+
+        memset(buf, 0, sizeof(buf));
+#ifdef USING_V_AXIS
+        sprintf(buf, "adc1: %5d | motor h: %5d | left: %d | right: %d | "
+                "adc2: %5d | motor v: %5d | top: %d | bot: %d\r\n",
+                adc1_val, speed_h, switch_hl, switch_hr,
+                adc2_val, speed_v, switch_vl, switch_vr);
+#else
+        sprintf(buf, "adc1: %5d | motor h: %5d | left: %d | right: %d\r\n",
+                adc1_val, speed_h, switch_hl, switch_hr);
+#endif
+        USART_Transmit(&huart2, buf);
     }
 }
 
+int32_t SpeedFilteringWithADCValue(int32_t adc) {
+  if (adc < 150) return -20000;
+  else if (adc > 3000) return 20000;
+  else return 0;
+}
 static void Error_Handler(void) {
     /* Turn LED2 on */
     BSP_LED_On(LED2);
